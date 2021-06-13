2b32149f71eeb63bd1fa249a3392e6ae58c041f9
Fabian Lopez <sektor86@gmail.com>
2021-06-13 00:03:39 -0400

    Documentado con Swagger

--- books/src/main/java/org/flopez/escalab/books/controller/LibroRestController.java
+++ books/src/main/java/org/flopez/escalab/books/controller/LibroRestController.java
@@ -1,5 +1,7 @@
 package org.flopez.escalab.books.controller;
 
+import java.util.List;
+
 import org.flopez.escalab.books.model.Libro;
 import org.flopez.escalab.books.response.LibroResponseRest;
 import org.flopez.escalab.books.service.ILibroService;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/libros")
 public class LibroRestController {
@@ -22,6 +26,10 @@ public class LibroRestController {
 	@Autowired
 	private ILibroService service;
 	
+	@ApiOperation(value = "Obtiene todos los libros",
+		      notes = "No requiere nada como parametro",
+		      response = List.class,
+		      responseContainer = "Libros")
 	@GetMapping
 	public ResponseEntity<LibroResponseRest> findLibros() {
 		
@@ -29,6 +37,10 @@ public class LibroRestController {
 		return response;
 	}
 	
+	@ApiOperation(value = "Obtiene el libro por id",
+		      notes = "Requiere un id de un libro como parametr",
+		      response = List.class,
+		      responseContainer = "Libros")
 	@GetMapping("/{id}")
 	public ResponseEntity<LibroResponseRest> consultaPorId(@PathVariable("id") Integer id) {
 		
@@ -36,12 +48,20 @@ public class LibroRestController {
 		return response;
 	}
 	
+	@ApiOperation(value = "Crea un nuevo libro",
+		      notes = "No requiere nada como parametro",
+		      response = List.class,
+		      responseContainer = "Libros")
 	@PostMapping
 	public ResponseEntity<LibroResponseRest> crear(@RequestBody Libro request) {
 		ResponseEntity<LibroResponseRest> response = service.crearLibro(request);
 		return response;
 	}
 	
+	@ApiOperation(value = "Actualiza un libro",
+		      notes = "Requiere un id de un libro como parametr",
+		      response = List.class,
+		      responseContainer = "Libros")
 	@PutMapping("/{id}")
 	public ResponseEntity<LibroResponseRest> actualizar(@RequestBody Libro request, @PathVariable Integer id) {
 		
@@ -49,6 +69,10 @@ public class LibroRestController {
 		return response;
 	}
 	
+	@ApiOperation(value = "Elimina un libro",
+		      notes = "Requiere un id de un libro como parametro",
+		      response = List.class,
+		      responseContainer = "Libros")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<LibroResponseRest> eliminar(@PathVariable Integer id) {
 		
